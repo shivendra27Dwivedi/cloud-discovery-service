f3143035c6fa11000b3154dc98333722b1353e3d
Shivendra Dwivedi <43295915+shivendra27Dwivedi@users.noreply.github.com>
2024-06-27 16:48:47 +0530

    Update JobRepository.java

--- nimesa/src/main/java/com/nimesa/task/repository/JobRepository.java
+++ nimesa/src/main/java/com/nimesa/task/repository/JobRepository.java
@@ -10,6 +10,5 @@ public interface JobRepository extends JpaRepository<Job, Long> {
     List<Job> findByJobId(String jobId);
     Job findByJobIdAndService(String jobId,String service);
     List<Job> findByService(String service);
-	Integer countByBucketNameAndStatus(String bucketName, String string);
-	List<Job> findByServiceAndStatus(String string, String string2);
+    List<Job> findByServiceAndStatus(String string, String string2);
 }
